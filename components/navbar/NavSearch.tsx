0a4bc9de8c8a27f20515bc93424c88992fc549f6
hajarhuseynova <hajarih@code.edu.az>
2025-04-22 15:34:57 +0400

    Initialize project

---
+++ components/navbar/NavSearch.tsx
@@ -0,0 +1,12 @@
+import { Input } from '../ui/input'
+
+function NavSearch() {
+  return (
+    <Input
+      type="search"
+      placeholder="Search Product..."
+      className="max-w-xs dark:bg-muted "
+    />
+  )
+}
+export default NavSearch
