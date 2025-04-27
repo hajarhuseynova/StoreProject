4a45214ebbeb1f69d2f66ff49ffefef68d8cae3e
hajarhuseynova <hajarih@code.edu.az>
2025-04-27 11:48:27 +0400

    ADD: Home page and Database

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,16 @@
+import LoadingContainer from '@/components/global/LoadingContainer'
+import FeaturedProducts from '@/components/home/FeaturedProducts'
+import Hero from '@/components/home/Hero'
+import { Suspense } from 'react'
+
 const HomePage = () => {
-  return <div>Store Starter</div>;
-};
-export default HomePage;
+  return (
+    <>
+      <Hero />
+      <Suspense fallback={<LoadingContainer />}>
+        <FeaturedProducts />
+      </Suspense>
+    </>
+  )
+}
+export default HomePage
