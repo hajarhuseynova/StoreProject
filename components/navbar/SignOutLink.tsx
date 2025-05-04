0a6e533986c72eb093f3aa438b7a07ba868912f8
hajarhuseynova <hajarih@code.edu.az>
2025-05-04 15:49:01 +0400

    ADD: Authentication

--- components/navbar/SignOutLink.tsx
+++ components/navbar/SignOutLink.tsx
@@ -1,7 +1,20 @@
-import React from 'react'
+'use client'
+import { SignOutButton } from '@clerk/nextjs'
+import { useToast } from '../ui/use-toast'
+import Link from 'next/link'
 
 function SignOutLink() {
-  return <div></div>
+  const { toast } = useToast()
+  const handleLogOut = () => {
+    toast({ description: 'Log Out Successfully' })
+  }
+  return (
+    <SignOutButton>
+      <Link href="/" className="w-full text-left" onClick={handleLogOut}>
+        Log Out
+      </Link>
+    </SignOutButton>
+  )
 }
 
 export default SignOutLink
